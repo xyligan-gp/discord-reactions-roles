155d53b80e40cd4e01ee6c4b77dc29b64dd7ae95
xyligan <48912672+xyligan-gp@users.noreply.github.com>
2023-06-25 17:19:57 +0300

    init

---
+++ assets/index-982d5085.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Spinner-118967e8.js";import{d as o,B as r,o as t}from"./index-532487fa.js";import"./ReducedMotion-22abd2ab.js";const p=o({__name:"index",setup(a){return(n,_)=>(t(),r(e))}});export{p as default};
